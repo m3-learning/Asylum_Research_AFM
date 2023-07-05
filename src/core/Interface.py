1a825c58d02420ecae7e13c5b1d387cba00c53c2
Yael Passy <ysp28@drexel.edu>
2023-07-05 14:51:25 -0400

    minor updates

--- src/core/Interface.py
+++ src/core/Interface.py
@@ -1,22 +1,25 @@
+from subprocess import Popen, TimeoutExpired
+
 class AFM:
 
-    def __init__(self, igor_path=r"C:\AsylumResearch\v18\Igor Pro Folder\Igor.exe", basepath='./', filename='ToIgor'):
+    def __init__(self, igor_path=r"C:\AsylumResearch\v19\RealTime\Igor Pro Folder\Igor.exe", basepath='./', filename='ToIgor', verbose = False):
 
         self.basepath = basepath
-        self.igor_path = igor_path.replace("/", "\\")
+        self.igor_path = igor_path
         self.filename = filename
-        self.bat = f"{self.cmd_basepath}\\{self.filename}.bat"
-        self.arcmd = f"{self.cmd_basepath}\\{self.filename}.arcmd"
+        self.bat = f"{self.cmd_basepath}\\\\{self.filename}.bat"
+        self.arcmd = f"{self.cmd_basepath}\\\\{self.filename}.arcmd"
+        self.verbose = verbose
 
         self.write_bat()
 
     @property
     def cmd_basepath(self):
-        return self.basepath.replace("/", "\\")
+        return self.basepath.replace("\\", "\\\\")
 
     @property
     def cmd_igor_path(self):
-        return self.igor_path.replace("/", "\\")
+        return self.igor_path.replace("\\", "\\\\")
 
     def write_file(self, file, content):
         
@@ -28,10 +31,22 @@ class AFM:
                 f.write(line + '\n')
                 
     def write_bat(self):
-        
-        content = f'@echo off\n\"{self.igor_path}\" \"{self.bat}\"'
+        arcmd = self.arcmd.replace("\\\\","\\")
+        content = f'@echo off\n\"{self.igor_path}\" \"{arcmd}\"'
         self.write_file(self.bat, content)
         
     def write_arcmd(self, content):
         self.write_file(self.arcmd, content)
+
+    def send_command(self):
+        p = Popen(self.bat.replace("\\\\","\\"))
+        try:
+            outs, errs = p.communicate(timeout=15)
+        except TimeoutExpired:
+            p.kill()
+            outs, errs = p.communicate()
+        
+        if self.verbose:
+            return outs, errs
+
     
