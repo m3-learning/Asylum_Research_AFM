f53e46378e95083f3f1544f9f388b81f956a9a12
Yael Passy <ysp28@drexel.edu>
2023-08-09 13:39:39 -0400

    scan grid stuff
    Co-authored-by: Joshua C Agar <jca318@lehigh.edu>

--- src/Controls/master_panel.py
+++ src/Controls/master_panel.py
@@ -48,24 +48,55 @@ _possible_PopupForce = Literal["DwellRampMode", "DwellSetting", "ImagingMode", "
 class MainPanel(AFM):
     command_list: list = dc.field(default_factory=list)
     script: bool = True
-        
+
     def SetValue(self, variable, value):
         return f'PV(\"{variable}\",{value})'
-    
+
     def ChangePopupImage(self, variable, value):
         return f'PopupMenu {variable}Popup_0 mode={value}'
-      
+
     def ChangePopupForce(self, variable, value):
         return f'PopupMenu {variable}Popup_1 mode={value}'
 
+    def igor_spot_maker(self, image_posx,image_posy):
+        return f'ARGo2ImagePos({image_posx},{image_posy})'
+
+    def draw_spot(self):
+        return f'DrawSpot(\"Draw\")'
+
+    def clear_spot(self):
+        return f'DrawSpot(\"Clear\")'
+
+    def change_force_spot(self, force_spot , value):
+        return f'PV(\"{force_spot}\", {value})'
+
+    def go_to_spot(self):
+        return f'GoToSpot()'
+
     def update_params(self: Type['MainPanel'], param: _possible_params, value: Any):
-        self.on_update(self.SetValue(param, value))  
+        self.on_update(self.SetValue(param, value))
 
     def update_PopupImage(self: Type['MainPanel'], param: _possible_PopupImage, value: Any):
-        self.on_update(self.ChangePopupImage(param, value))  
+        self.on_update(self.ChangePopupImage(param, value))
 
     def update_PopupForce(self: Type['MainPanel'], param: _possible_PopupForce, value: Any):
-        self.on_update(self.ChangePopupForce(param, value))  
+        self.on_update(self.ChangePopupForce(param, value))
+
+    def update_spot(self: Type['MainPanel'], image_posx: Any, image_posy:Any):
+        self.on_update(self.igor_spot_maker(image_posx,image_posy))
+
+    def draw_update(self: Type['MainPanel']):
+        self.on_update(self.draw_spot())
+
+    def clear_update(self: Type['MainPanel']):
+        self.on_update(self.clear_spot())
+
+    def update_location(self: Type['MainPanel'], force_spot, value:Any ):
+        self.on_update(self.change_force_spot(force_spot,value))
+
+    def move_location(self: Type['MainPanel']):
+        self.on_update(self.go_to_spot())
+
 
     def on_update(self, str_update):
         if self.script:
@@ -74,7 +105,6 @@ class MainPanel(AFM):
             self.write_arcmd(str_update)
             self.send_command()
 
-    
+
     # TODO add setter and return statement for command list
 
-     
---
+++ src/Controls/scans.py
@@ -0,0 +1,93 @@
+import time
+import tempfile
+
+import numpy as np
+
+from ..core.Interface import AFM
+from ..Controls.master_panel import MainPanel
+
+
+class GridScan():
+
+    def __init__(self,
+                 num_x_grid_points,
+                 num_y_grid_points,
+                measurement= None,
+                 igor_path=r"C:\AsylumResearch\v19\RealTime\Igor Pro Folder\Igor.exe", basepath=None, filename='ToIgor', verbose = False):
+
+        self.num_x_grid_points = num_x_grid_points
+        self.num_y_grid_points = num_y_grid_points
+        self.main_panel = MainPanel(script=True, igor_path=igor_path, basepath=basepath, filename=filename, verbose=verbose)
+        self._raw_grid = ()
+        self.numbered_grid: list[dict[int, tuple]] = []
+
+        # a measurement class
+        self.measurement = measurement
+
+
+    @property
+    def num_x_grid_points(self):
+        return self._num_x_grid_points
+
+    @num_x_grid_points.setter
+    def num_x_grid_points(self, value):
+        if value is not None:
+            self._num_x_grid_points = value
+
+    @property
+    def num_y_grid_points(self):
+        return self._num_y_grid_points
+
+    @num_y_grid_points.setter
+    def num_y_grid_points(self, value):
+        if value is not None:
+            self._num_y_grid_points = value
+
+
+    def make_grid(self):
+        self._raw_grid = None
+        scansize = self.main_panel.get_params(["ScanSize"], "MasterVariablesWave","root:packages:MFP3D:main:variables")
+
+        # Create arrays of cell borders using linspace with the specified number of points
+        x = np.linspace(0, scansize, self.num_x_grid_points)
+        y = np.linspace(0, scansize, self.num_y_grid_points)
+
+        # Create a grid
+        X, Y = np.meshgrid(x, y)
+
+        self._raw_grid = zip(X.flatten(), Y.flatten())
+
+    def _create_force_spot(self, x: float, y: float) -> int:
+        self.main_panel.update_spot(x,y)
+        self.main_panel.draw_update()
+        #self.main_panel.execute()
+        #spotnumber = self.main_panel.get_params(["ForceSpotNumber"], "ForceVariablesWave","root:packages:MFP3D:main:variables")
+        #return spotnumber
+
+
+    def create_grid_on_igor(self, sleep_time=0):
+        for X, Y in self._raw_grid:
+            self._create_force_spot(X,Y)
+            self.numbered_grid.append({len(self.numbered_grid)+1, (X, Y)})
+            time.sleep(sleep_time)
+
+    def clean_up(self):
+        self.main_panel.clear_update()
+        self.main_panel.execute()
+
+    # Generator
+[ measurement_object, spot number]
+    def run_scan(self):
+        scan_generator = self.dict_generator(self.numbered_grid)
+
+            # TODO Go there
+            ## TODO get the z value
+            # TODO Dwell
+            # TODO Run_Measurement
+
+    @staticmethod
+    def dict_generator(list_):
+        for list_ in list_:
+            yield list_
+
+
